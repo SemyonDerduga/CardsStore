61b9c073749577bd8f672b00b48883bb0c14db91
semenderduga <semenderduga@gmail.com>
2018-10-12 21:40:14 +0300

    Add configuration from terminal

--- cardsstore/app.py
+++ cardsstore/app.py
@@ -3,6 +3,9 @@ from .routes import setup_routes
 import jinja2
 import aiohttp_jinja2
 
+
+
+
 async def create_app():
     app = web.Application()
     aiohttp_jinja2.setup(
--- entry.py
+++ entry.py
@@ -1,8 +1,29 @@
+import asyncio
 import aiohttp
+import argparse
+import aioreloader
 from cardsstore import create_app
+
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    print("Library uvloop is not available")
+
+
+parser = argparse.ArgumentParser(description="Cards Store")
+parser.add_argument('--host', help="Host to listen",default="0.0.0.0")
+parser.add_argument('--port', help="Port to connections",default="8080")
+parser.add_argument('--reload', action="store_true",
+                    help="Autoreload code on change")
+
+args = parser.parse_args()
 
 app = create_app()
 
-if __name__ == '__main__':
-    aiohttp.web.run_app(app, )
+if args.reload:
+    print("Start with reload code")
+    aioreloader.start()
 
+if __name__ == '__main__':
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
