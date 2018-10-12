84045d4b43b94f6899d841d1593f2c94532d7743
semenderduga <semenderduga@gmail.com>
2018-10-12 23:25:12 +0300

    add config file

--- cardsstore/app.py
+++ cardsstore/app.py
@@ -4,13 +4,12 @@ import jinja2
 import aiohttp_jinja2
 
 
-
-
-async def create_app():
+async def create_app(config: dict):
     app = web.Application()
+    app['config'] = config
     aiohttp_jinja2.setup(
         app,
-        loader = jinja2.PackageLoader('cardsstore', 'templates')
+        loader=jinja2.PackageLoader('cardsstore', 'templates')
     )
     setup_routes(app)
     return app
--- entry.py
+++ entry.py
@@ -2,6 +2,7 @@ import asyncio
 import aiohttp
 import argparse
 import aioreloader
+from cardsstore.settings import load_config
 from cardsstore import create_app
 
 try:
@@ -12,14 +13,16 @@ except ImportError:
 
 
 parser = argparse.ArgumentParser(description="Cards Store")
-parser.add_argument('--host', help="Host to listen",default="0.0.0.0")
-parser.add_argument('--port', help="Port to connections",default="8080")
+parser.add_argument('--host', help="Host to listen", default="0.0.0.0")
+parser.add_argument('--port', help="Port to connections", default="8080")
 parser.add_argument('--reload', action="store_true",
                     help="Autoreload code on change")
+parser.add_argument("-c", "--config", type=argparse.FileType('r'),
+                    help="Path to configuration file")
 
 args = parser.parse_args()
 
-app = create_app()
+app = create_app(config=load_config(args.config))
 
 if args.reload:
     print("Start with reload code")
